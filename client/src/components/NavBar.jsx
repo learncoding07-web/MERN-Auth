84b18b90b09addfb14b8ac5da757f943bca6f842
JoshiKrish07 <krishnakant0795@gmail.com>
2024-08-09 14:38:22 +0530

    added navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='bg-slate-200'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+      <Link to='/'><h1 className='font-bold '>Auth App</h1></Link>
+        <ul className='flex gap-4'>
+        <Link to='/'><li>Home</li></Link>
+        <Link to='/about'><li>About</li></Link>
+        <Link to='/signin'><li>Sign In</li></Link>
+        </ul>
+      </div>
+    </div>
   )
 }
 
